9978b65d6dbb596b37ef62ffbb68f32e1da5107f
ashfaq1808 <ashfaqpasha66@gmail.com>
2022-08-12 21:20:16 +0530

    first commit

---
+++ routes/tasks.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Router = express.Router();
+
+const { getAllTasks, 
+    createTask, 
+    getTask, 
+    updateTask, 
+    deleteTask } = require('../controllers/tasks')
+
+Router.route('/').get(getAllTasks).post(createTask)
+Router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+module.exports = Router
